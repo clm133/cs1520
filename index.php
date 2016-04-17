c0269aeeae9831db45378f8ce1b35b841b69ef02
clm133 <charliemietzner@gmail.com>
2016-04-17 17:00:17 -0400

    ok, getting the hang of git. Test succeeded

--- index.php
+++ index.php
@@ -20,7 +20,6 @@
 			include("initiative/display_order.php");
 			include("initiative/add_to_order.php");
 			include("initiative/start_new_order.php");
-			echo "<p>this is a change to test github commit</p>";
 		?>
 		</div>
 	<script src="js/jquery-1.12.1.min.js"></script>
